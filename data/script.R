a83ea97f09f5a3a03d1c2d9714d98e6288eec882
Rodslater <91638825+Rodslater@users.noreply.github.com>
2023-04-12 17:10:25 -0300

    Update script.R

--- data/script.R
+++ data/script.R
@@ -1,11 +1,9 @@
 ## Preparação do histórico ## 
 library(rinmet)
 library(tidyverse)
-library(lubridate)
 library(rio)
 
 ##########
-#datas <- 2017:2022
 datas <- 2023:year(Sys.Date())
 for(i in seq_along(datas)) {
   try({tempo <- get_base_inmet(datas[i]) #Try passa pra o próximo em caso de erro.
@@ -18,6 +16,10 @@ tempo <- import_list(dir(pattern = ".csv"), rbind = TRUE, encoding = "UTF-8")
 tempo <- tempo[,c(1:3, 11, 10, 21, 22)]
 colnames(tempo) <- c("data", "hora", "precipitacao_total", "temp_min", "temp_max", "uf", "estacao")
 
+arquivos_csv <- dir(pattern = ".csv")
+if (file.exists(arquivos_csv)) {
+  file.remove(arquivos_csv)
+}
 
 tempo <- tempo %>% 
   filter(uf=="SE") %>% 
@@ -32,9 +34,4 @@ tempo <- tempo %>%
          estacao = case_when(estacao == 'Poco Verde' ~ 'Poço Verde',
                              estacao == 'Nossa Senhora Da Gloria' ~ 'Nossa Senhora da Glória',
                              TRUE ~ estacao))
-saveRDS(tempo, paste0("tempo_", datas[1], "a", datas[length(datas)], ".rds"))
-
-arquivos_csv <- dir(pattern = ".csv")
-if (file.exists(arquivos_csv)) {
-  file.remove(arquivos_csv)
-}
+saveRDS(tempo, 'tempo.rds')
