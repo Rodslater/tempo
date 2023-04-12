022b7a384c316e97bf0a1a195d52090f8b879404
Rodslater <91638825+Rodslater@users.noreply.github.com>
2023-04-12 14:58:37 -0300

    Update script.R

--- data/script.R
+++ data/script.R
@@ -4,6 +4,11 @@ library(tidyverse)
 library(lubridate)
 library(rio)
 
+arquivos_rds <- dir(pattern = ".rds")
+if (file.exists(arquivos_rds)) {
+  file.remove(arquivos_rds)
+}
+
 ##########
 datas <- 2023:year(Sys.Date())
 for(i in seq_along(datas)) {
@@ -31,26 +36,18 @@ tempo <- tempo %>%
          estacao = case_when(estacao == 'Poco Verde' ~ 'Poço Verde',
                              estacao == 'Nossa Senhora Da Gloria' ~ 'Nossa Senhora da Glória',
                              TRUE ~ estacao))
-saveRDS(tempo, paste0("bases/tempo_", datas[1], "a", datas[length(datas)], ".rds"))
+saveRDS(tempo, paste0("tempo_", datas[1], "a", datas[length(datas)], ".rds"))
+
+
+##########
+
+#Juntando bases
+tempo <- import_list(dir(pattern = ".rds"), rbind = TRUE)
+tempo <- tempo %>% select(-`_file`)
 
 arquivos_rds <- dir(pattern = ".rds")
 if (file.exists(arquivos_rds)) {
   file.remove(arquivos_rds)
 }
-
-##########
-
-
-
-
-#Juntando bases
-
-diretorio <- getwd()
-novo_diretorio <- paste0(diretorio, "/bases/")
-setwd(novo_diretorio)
-#tempo <- import_list(dir(path = "/bases/", pattern = ".rds"), rbind = TRUE) #tá dando erro qd tenta acessar dentro da pasta. na raiz fica normal
-tempo <- import_list(dir(pattern = ".rds"), rbind = TRUE)
-tempo <- tempo %>% select(-`_file`)
-setwd(diretorio)
 
 saveRDS(tempo, paste0("tempo_", year(min(tempo$data)), "a", year(max(tempo$data)), ".rds"))
